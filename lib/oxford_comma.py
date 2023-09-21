ca2a26884af21e1d936569800be3a2bc4aca45cf
steve-the-hermit <stevemacharia309@gmail.com>
2023-09-22 00:36:20 +0300

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+    if len(elements) == 0:
+        return ""
+    elif len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return f"{elements[0]} and {elements[1]}"
+    else:
+        # Join all elements with commas, except for the last one, which is preceded by "and"
+        comma_separated = ", ".join(elements[:-1])
+        return f"{comma_separated}, and {elements[-1]}"
